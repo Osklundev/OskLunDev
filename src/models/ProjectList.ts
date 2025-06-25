8f59f6c2ca9ddb0c9a5518f9a73d9988d8a2a19c
OskLunDev <Oskarlundberg@hotmail.com>
2025-06-25 20:19:31 +0200

    Fix: Image imports

--- src/models/ProjectList.ts
+++ src/models/ProjectList.ts
@@ -1,9 +1,12 @@
 import type { Project } from "./Project";
+import JurassicSite from "../assets/JurassicSite.jpg";
+import QuizSite from "../assets/QuizSite.jpg";
+import TodoSite from "../assets/TodoSite.jpg";
 
 export const projectList: Project[] = [
   {
     title: "Jurassic",
-    img: "https://media.discordapp.net/attachments/82814527707549696/1386980380032237649/image.png?ex=685bada3&is=685a5c23&hm=a7bb826881a50311ac7195a8f817665e2d5b4d8109315f4b273583e9c09ce011&=&format=webp&quality=lossless&width=1409&height=670",
+    img: JurassicSite,
     url: "https://medieinstitutet.github.io/fed24d-grafiska-verktyg-kejsartamarinerna/",
     description:
       "School project where we were divided into groups, and first had to design a project and then exchange our designs with another group and create the website for whatever design we were handed",
@@ -11,7 +14,7 @@ export const projectList: Project[] = [
   },
   {
     title: "Quiz",
-    img: "https://media.discordapp.net/attachments/82814527707549696/1387035638779482144/image.png?ex=685be11a&is=685a8f9a&hm=9949f0a0878a877f1c13ac67e5b579ffc11cd0b0a8ace21b167743a31d538e8f&=&format=webp&quality=lossless&width=1411&height=670",
+    img: QuizSite,
     url: "https://medieinstitutet.github.io/fed24d-js-intro-inl-2-quiz-ballerinas/",
     description:
       "School project to create a quiz, we chose a quiz about the ballerina cookie, once again group project",
@@ -19,7 +22,7 @@ export const projectList: Project[] = [
   },
   {
     title: "Todo",
-    img: "https://media.discordapp.net/attachments/82814527707549696/1387043653129736282/image.png?ex=685be890&is=685a9710&hm=4acff19fa173331cdaf689fe6857dc52960876bc9eec4601ffee9f8baf0142c6&=&format=webp&quality=lossless&width=1411&height=670",
+    img: TodoSite,
     url: "https://medieinstitutet.github.io/fed24d-the-last-todos-Osklundev/",
     description: "School assignment to create a todo list in react",
     current: false,
