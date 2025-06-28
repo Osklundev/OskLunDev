4db468b2000cbe4b1188a8dae15686d778165408
OskLunDev <Oskarlundberg@hotmail.com>
2025-06-28 16:32:37 +0200

    WIP Styling desktop

--- src/components/PortfolioBody.tsx
+++ src/components/PortfolioBody.tsx
@@ -2,12 +2,15 @@ import { PortfolioProjects } from "./PortfolioProjects";
 
 export const PortfolioBody = () => {
   return (
-    <div id="body" className="min-h-screen flex flex-col gap-4">
+    <div
+      id="body"
+      className="min-h-screen md:flex md:flex-col md:justify-between"
+    >
       <section>
         <h2 className="text-center font-bold border-b-2 max-w-30 m-auto">
           Who am I?
         </h2>
-        <div className="p-2 flex flex-column min-w-full border-b-5">
+        <div className="p-2 flex md:max-w-1/2 md: m-auto">
           <div className="min-w-1/2 text-center">
             <h3 className="font-bold underline">Technologies</h3>
             <ul>
@@ -25,7 +28,7 @@ export const PortfolioBody = () => {
               <li>MongoDB</li>
             </ul>
           </div>
-          <div className="min-w-1/2 text-center flex flex-col justify-around">
+          <div className="text-center flex flex-col justify-around">
             <p>
               I am a Frontend Development student at Medieinstitutet, graduating
               spring 2026.
--- src/components/PortfolioProjects.tsx
+++ src/components/PortfolioProjects.tsx
@@ -2,19 +2,25 @@ import { projectList } from "../models/ProjectList";
 
 export const PortfolioProjects = () => {
   return (
-    <section className="flex-col">
-      {projectList.map((p) => (
-        <div className="m-5 border-b-6 border-b-amber-500">
-          <h3 className="font-bold underline text-center">Projects</h3>
-          <a href={p.url} target="_blank">
-            <img src={p.img} alt={p.title} />
-          </a>
-          <h4 key={p.title} className="font-bold">
-            {p.title}
-          </h4>
-          <p>{p.description}</p>
-        </div>
-      ))}
+    <section>
+      <h3 className="text-center font-bold border-b-2 max-w-30 m-auto">
+        Projects
+      </h3>
+      <div className="md:flex">
+        {projectList.map((p) => (
+          <div className="m-5 border-b-6 border-b-amber-500">
+            <a href={p.url} target="_blank">
+              <img src={p.img} alt={p.title} className="md:max-w-96" />
+            </a>
+            <div>
+              <h4 key={p.title} className="font-bold md:text-center">
+                {p.title}
+              </h4>
+              <p className="md:text-center">{p.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
