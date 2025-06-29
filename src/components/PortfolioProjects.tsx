02ecf3214f183d8a0de493d7c29c28ed486fd024
OskLunDev <Oskarlundberg@hotmail.com>
2025-06-29 14:25:04 +0200

    styling changes, icons for technologies

--- src/components/PortfolioProjects.tsx
+++ src/components/PortfolioProjects.tsx
@@ -2,13 +2,13 @@ import { projectList } from "../models/ProjectList";
 
 export const PortfolioProjects = () => {
   return (
-    <section className="col-start-3 col-end-5 overflow-auto md:max-h-screen">
+    <section className="col-start-3 col-end-5 overflow-auto md:max-h-screen mt-10">
       <div>
-        <h2 className="text-center font-bold underline max-w-30 m-auto">
+        <h2 className="text-center font-bold underline m-5 mb-0 bg-stone-900 m-5 drop-shadow-xl drop-shadow-red-500/75 border-2 border-red-400 border-b-0">
           Projects
         </h2>
         {projectList.map((p) => (
-          <div className="bg-stone-900 m-5 drop-shadow-xl drop-shadow-red-500/75 border-2 border-red-400">
+          <div className="bg-stone-900 m-5 mt-0 drop-shadow-xl drop-shadow-red-500/75 border-2 border-red-400">
             <a href={p.url} target="_blank">
               <img src={p.img} alt={p.title} className="md:max-w-full" />
             </a>
